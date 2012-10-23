aa52d9f3700028d9255c24acd92c38e7e37256a5
Mikk Kirstein <mikk@kolmas.org>
2012-10-23 19:33:12 +0300

    improved test

--- test/module.utils.js
+++ test/module.utils.js
@@ -2,6 +2,7 @@ var libpath = process.env['COLLABISTIC_COV'] ? '../lib-cov' : '../lib',
     should  = require('should'),
     rimraf  = require('rimraf'),
     path    = require('path'),
+    fs      = require('fs');
     mkdirp  = require('mkdirp');
 
 var utils   = require(path.join(libpath, 'module.utils'));
@@ -9,23 +10,25 @@ var utils   = require(path.join(libpath, 'module.utils'));
 describe('module.utils', function() {
     var mockDirName     = 'mock',
         mockModuleName  = mockDirName;
+        fileName        = 'index.js';
         mockDir         = path.join(__dirname, mockDirName, mockModuleName);
 
     before(function() {
         mkdirp.sync(mockDir);
+        fs.writeFileSync(path.join(mockDir, fileName), '');
     });
 
     after(function() {
         rimraf.sync(path.join(__dirname, mockDirName));
     });
 
-    describe('#getModulesSync(dir, [options])', function() {
+    describe('#getModulesSync(dir, [files])', function() {
         it('should throw exception when no path is set', function() {
             (function() {
                 utils.getModulesSync();
             }).should.throw("module directory path not defined or does not exist");
         });
-        it('should return mock module', function() {
+        it('should return mock module with no files set', function() {
             var modulePath = path.join(__dirname, mockDirName);
 
             utils.getModulesSync(modulePath)
@@ -33,5 +36,16 @@ describe('module.utils', function() {
                  .with.lengthOf(1)
                  .and.include(mockDirName);
         });
+        it('should return mock module when files listing is set', function() {
+
+        });
+
+        it('should return empty array when files listing is set but module does not match', function() {
+            var modulePath = path.join(__dirname, mockDirName);
+
+            utils.getModulesSync(modulePath, [ 'does not exist' ])
+                 .should.be.an.instanceOf(Array)
+                 .with.lengthOf(0)
+        })
     });
 });
