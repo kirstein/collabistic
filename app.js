0588a14549ff5416bf0cbc5abdea330766abf021
Mikk Kirstein <mikk@kolmas.org>
2012-11-04 13:51:06 +0200

    Added functionality to app

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-var config = require('./config'),
-    path   = require('path');
+var config  = require('./config'),
+    path    = require('path'),
+    http    = require('http'),
+    app     = require(path.join(config.appURI, 'app'));
 
-// app variables
-var app = require(path.join(config.appURI, 'app')),
-    http   = require('http');
+var manager = require(path.join(config.appURI, 'module-manager'));
+
+// Global object
+global.collabistic         = { };
+global.collabistic.app     = app;
+global.collabistic.modules = manager.loadModules();
+
+require(config.routerURI)();
 
 http.createServer(app).listen(app.get('port'), function(){
     console.log("Express app listening on port " + app.get('port'));
