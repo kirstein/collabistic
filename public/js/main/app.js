6d79ea77f65978f331e60564996e3bca2b3d113f
Mikk Kirstein <mikk@kolmas.org>
2012-11-09 20:20:31 +0200

    Test public

---
+++ public/js/main/app.js
@@ -0,0 +1,5 @@
+requirejs(['jquery'], function($) {
+    //jQuery, canvas and the app/sub module are all
+    //loaded and can be used here now.
+    console.log($);
+});
---
+++ public/js/main/config.js
@@ -0,0 +1,6 @@
+requirejs.config({
+    baseUrl: '../',
+    paths: {
+        lib: '/lib'
+    }
+});
