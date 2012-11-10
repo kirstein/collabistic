8e2d2b2272024d5a1d2be1e3c1cd615d2706abb5
Mikk Kirstein <mikk@kolmas.org>
2012-11-11 00:47:09 +0200

    reworked mixin loading logic

--- public/js/main/mixins.js
+++ public/js/main/mixins.js
@@ -1,40 +1,54 @@
-var mixins = (function() {
-    var res    = [],
-        mixins = window.config.mixins,
-        i, mixin, config;
+define(function() {
+    var getMixins = function() {
+        var res = [],
+            mixins = window.config.mixins,
+            i, mixin, config;
 
-    for (i = 0; i < mixins.length; i++) {
-        mixin = mixins[i];
+        for(i = 0; i < mixins.length; i++) {
+            mixin = mixins[i];
 
-        // Check if module definition is an object
-        // If its an object check if its has a path
-        if (typeof mixin === 'object' && typeof mixin.path !== 'undefined') {
-            config        = {};
-            config[mixin.path] = mixin.config;
+            // Check if module definition is an object
+            // If its an object check if its has a path
+            if(typeof mixin === 'object' && typeof mixin.path !== 'undefined') {
+                config = {};
+                config[mixin.path] = mixin.config;
 
-            // If module has config attach it.
-            if (typeof mixin.config !== 'undefined') {
-                requirejs.config({ config : config });
+                // If module has config attach it.
+                if(typeof mixin.config !== 'undefined') {
+                    requirejs.config({
+                        config: config
+                    });
+                }
+                res.push(mixin.path);
+            } else {
+                res.push(mixin);
             }
-            res.push(mixin.path);
-        } else {
-            res.push(mixin);
         }
+        return res;
+    };
+
+    var loadMixin = function(mixin) {
+        var loadingSuccess, loadingFail;
+
+        loadingSuccess = function() {
+            console.info("Mixin:", mixin, "loaded!");
+        };
+
+        loadingFail = function(err) {
+            var failedId = err.requireModules && err.requireModules[0];
+            console.error('Mixin loading failed:', failedId);
+        };
+
+        require([mixin], loadingSuccess, loadingFail);
+    };
+
+    var mixins = getMixins(),
+        i;
+
+    for(i = 0; i < mixins.length; i++) {
+        loadMixin(mixins[i]);
     }
 
-    return res;
-})();
-
-// Boilerplate for loading all the mixins.
-require([].concat(mixins), function() {
-    var mixins = Array.prototype.slice.call(arguments);
-
-    console.info("Mixins loaded:", mixins.length);
-
-    // Return the list of loaded mixins
+    // Return the list of mixins
     return mixins;
-}, function (err) {
-    var failedId = err.requireModules && err.requireModules[0];
-    alert(err.requireModules);
-    console.error('Mixin loading failed:', failedId);
 });
