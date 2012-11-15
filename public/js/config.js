d7f8225e2cbfb104fe600085f76505431ca8e14b
Mikk Kirstein <mikk@kolmas.org>
2012-11-15 22:14:56 +0200

    Conf

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,6 @@
-window.config = {
-    initiators : 'initiators.json',
-    mixins: [{
+{
+    "initiators" : "initiators.json",
+    "mixins": [{
         // Logging levels:
         // 5. Error, someting really wrong, used data can be corrupt. Shit hit the fan.
         // 4. Warning, this is not right, I can continue, but please have a look.
@@ -9,21 +9,21 @@ window.config = {
         // 1. Log, almost everything...
         // 0. Everything!
         // All levels above are included. (lvl 3 = 3,4)
-        path: 'mixin/console',
-        config: {
-            dateFormat: '[%H:%m:%s]',
-            logLevel: 1
+        "path": "mixin/console",
+        "config": {
+            "dateFormat": "[%H:%m:%s]",
+            "logLevel": 1
         }
     }, {
-        path: 'mixin/pushState',
-        config: {
-            cookieName: 'redirect'
+        "path": "mixin/pushState",
+        "config": {
+            "cookieName": "redirect"
         }
     }, {
-        path: 'mixin/router',
-        config : {
-            pushState: true,
-            trigger  : true
+        "path": "mixin/router",
+        "config" : {
+            "pushState": true,
+            "trigger"  : true
         }
     }]
-};
+}
